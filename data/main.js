2c4e703294bdce99b76795bbd058435af8c8bcd4
wukddang <43228743+funkyblues@users.noreply.github.com>
2022-07-20 01:47:44 +0900

    Tile Layer Visualizing

--- data/main.js
+++ data/main.js
@@ -49,19 +49,84 @@ const ImageLayer = new ol.layer.Image({
 
 
 // Adding Layer to Map (After const map is defined)
-map.addLayer(ImageLayer)
+// map.addLayer(ImageLayer)
 
-// Image source for tiger
-const tigerSource = new ol.source.ImageStatic({
-  attribution: '<b>Tiger</b>',
-  url:'tiger.png',
-  imageExtent:[1231584.7236531826, 2612019.4933642442, 1289688.1854128095, 2657830.3699158556]
+// // Image source for tiger
+// const tigerSource = new ol.source.ImageStatic({
+//   attribution: '<b>Tiger</b>',
+//   url:'tiger.png',
+//   imageExtent:[1231584.7236531826, 2612019.4933642442, 1289688.1854128095, 2657830.3699158556]
+// })
+
+// // Tiger layer
+// const tigerLayer = new ol.layer.Image({
+//   source: tigerSource
+// })
+
+// // Add tiger layer map.
+// map.addLayer(tigerLayer)
+
+
+
+// Adding image based on coordinates
+// LAYER -> SOURCE -> FEATURE (based on coordinate) => Coordinates and Image
+
+// Defining a feature
+const Feature = new ol.Feature({
+  geometry : new ol.geom.Point([376957.5608535403, 2223378.7770221103]),
 })
 
-// Tiger layer
-const tigerLayer = new ol.layer.Image({
-  source: tigerSource
+const style = new ol.style.Style({
+  image : new ol.style.Icon({
+    src: 'tiger.png',
+    scale: 0.1
+  })
 })
 
-// Add tiger layer map.
-map.addLayer(tigerLayer)
+Feature.setStyle(style)
+
+// Define a source
+const vectorSource = new ol.source.Vector({
+  features: [Feature] 
+})
+
+// Define a layer
+
+const vectorLayer = new ol.layer.Vector({
+  source: vectorSource
+})
+
+// add this layer to map
+map.addLayer(vectorLayer)
+
+// Adding WMS layer to the map
+
+// Tile
+// Define WMS source
+const tileSource = new ol.source.TileWMS({
+  // url: '',
+  // params: {'LAYERS': 'tiger:poly_landmarks', 'TILED':true},
+  // serverType: 'geoserver',
+  // crossOrigin: 'anonymous'
+})
+
+// Define tile layer
+const tileLayer = new ol.layer.Tile({
+  source: tileSource
+})
+
+// Add WMS Layer to map
+map.addLayer(tileLayer)
+
+const imageSource = new ol.source.ImageWMS({
+  // url:,
+  // params: {'LAYERS': 'tiger:poly_landmarks'}
+  // serverType: 'geoserver',
+  // crossOrigin: 'anonymous'
+})
+
+const imageLayer = new ol.layer.Image({
+  source: imageSource
+})
+
+map.addLayer(imageLayer)
