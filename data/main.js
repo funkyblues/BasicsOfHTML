77d857b7c44841e7c61c3d0ff59846c76270839e
wukddang <43228743+funkyblues@users.noreply.github.com>
2022-07-20 22:31:19 +0900

    select만들고 타입 변경??

--- data/main.js
+++ data/main.js
@@ -22,251 +22,89 @@ var map = new ol.Map({
   view: view,
 });
 
-// var inputJSON = {
-//   "type": "FeatureCollection",
-//   "features": [
-//     {
-//       "type": "Feature",
-//       "properties": {},
-//       "geometry": {
-//         "type": "Polygon",
-//         "coordinates": [
-//           [
-//             [
-//               76.025390625,
-//               24.00632619875113
-//             ],
-//             [
-//               77.51953125,
-//               24.00632619875113
-//             ],
-//             [
-//               77.51953125,
-//               25.562265014427492
-//             ],
-//             [
-//               76.025390625,
-//               25.562265014427492
-//             ],
-//             [
-//               76.025390625,
-//               24.00632619875113
-//             ]
-//           ]
-//         ]
-//       }
-//     },
-//     {
-//       "type": "Feature",
-//       "properties": {},
-//       "geometry": {
-//         "type": "Polygon",
-//         "coordinates": [
-//           [
-//             [
-//               77.9150390625,
-//               28.69058765425071
-//             ],
-//             [
-//               77.3876953125,
-//               28.22697003891834
-//             ],
-//             [
-//               77.95898437499999,
-//               27.800209937418252
-//             ],
-//             [
-//               79.013671875,
-//               28.033197847676377
-//             ],
-//             [
-//               79.013671875,
-//               28.34306490482549
-//             ],
-//             [
-//               78.6181640625,
-//               28.729130483430154
-//             ],
-//             [
-//               77.9150390625,
-//               28.69058765425071
-//             ]
-//           ]
-//         ]
-//       }
-//     }
-//   ]
-// }
+// Drag box 
+const dragBox = new ol.interaction.DragBox({
 
-//   Vector source
-// var vectorSource = new ol.source.Vector({
-//     features : (new ol.format.GeoJSON().readFeatures(inputJSON))
-// })
+})
 
-// Vector Layer
-// var vectorLayer = new ol.layer.Vector({
-//     source: vectorSource,
-//     style: new ol.style.Style({
-//         stroke : new ol.style.Stroke({
-//             color : '#FF0000',
-//             width : 5,
-//             lineDash:[5,10]
-//         })
-//     })
+dragBox.on('boxstart', function(evt){
+  console.log('Box event started')
+})
 
-// })
+// Zoom to the extent of box at the end of the drag
+dragBox.on('boxend', function(evt) {
+  console.log('Box ended successfully')
+  map.getView().fit(dragBox.getGeometry().getExtent() , map.getSize());
+})
 
-// // Adding vector layer to map
-// // map.addLayer(vectorLayer)
+// map.addInteraction(dragBox)
 
-// // Adding vector layer from geojson file
-const extSource = new ol.source.Vector({
-  format: new ol.format.GeoJSON(),
-  url: "test.geojson",
-  //url : 'http://localhost:8080/geoserver/tiger/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=tiger%3Apoly_landmarks&maxFeatures=50&outputFormat=application%2Fjson'
-});
+// Drag and Drop Interaction
+const dragSource = new ol.source.Vector()
 
-const extLayer = new ol.layer.Vector({
-  source: extSource,
-  // style: new ol.style.Style({
-  //     fill : new ol.style.Fill({
-  //         color: 'rgba(142, 226, 136, 0.1)'
-  //     }),
-  //     stroke: new ol.style.Stroke({
-  //         color:'#000000',
-  //         width:3
-  //     })
-  // })
-  style: function (feature) {
-    if (feature.getProperties().prop == "elementaryschool") {
-      return new ol.style.Style({
-        fill: new ol.style.Fill({
-          color: "#ff0000",
-        }),
-        stroke: new ol.style.Stroke({
-          color: "#000000",
-          width: 3,
-        }),
-      });
-    } else if (feature.getProperties().prop == "university") {
-      return new ol.style.Style({
-        fill: new ol.style.Fill({
-          color: "#ffff00",
-        }),
-        stroke: new ol.style.Stroke({
-          color: "#000000",
-          width: 3,
-        }),
-      });
-    } else if (feature.getProperties().prop == "highway") {
-      return new ol.style.Style({
-        fill: new ol.style.Fill({
-          color: "#ffff00",
-        }),
-        stroke: new ol.style.Stroke({
-          color: "#ff22ff",
-          width: 5,
-        }),
-      });
-    } else if (feature.getProperties().prop == "lowway") {
-      return new ol.style.Style({
-        fill: new ol.style.Fill({
-          color: "#ffffff",
-        }),
-        stroke: new ol.style.Stroke({
-          color: "#33ff33",
-          width: 3,
-        }),
-      });
-    } else if (feature.getProperties().prop == "lab") {
-      return new ol.style.Style({
-        fill: new ol.style.Fill({
-          color: "#ffffff",
-        }),
-        stroke: new ol.style.Stroke({
-          color: "#33ff33",
-          width: 3,
-        }),
-      });
-    } else if (feature.getProperties().prop == "lake") {
-      return new ol.style.Style({
-        // fill: new ol.style.Fill({
-        //   color: "#ffffff",
-        // }),
-        stroke: new ol.style.Stroke({
-          color: "#33ff33",
-          width: 3,
-        }),
-      });
-    } else if (feature.getProperties().prop == "atm") {
-      return new ol.style.Style({
-        // fill: new ol.style.Fill({
-        //   color: "#ffffff",
-        // }),
-        // image: new Icon({
-        //   src: "flag.png",
-        // }),
-        image: new ol.style.Icon({
-          src: "flag.png",
-          scale: 0.05,
-        }),
-      });
-    } else if (feature.getProperties().prop == "labs") {
-      return new ol.style.Style({
-        // fill: new ol.style.Fill({
-        //   color: "#ffffff",
-        // }),
-        // image: new Icon({
-        //   src: "flag.png",
-        // }),
-        image: new ol.style.Icon({
-          src: "aa.png",
-          scale: 0.1,
-        }),
-      });
-    } else {
-      return new ol.style.Style({
-        fill: new ol.style.Fill({
-          color: "#000ff0",
-        }),
-        stroke: new ol.style.Stroke({
-          color: "#000000",
-          width: 3,
-        }),
-      });
+const dragLayer = new ol.layer.Vector({
+  source: dragSource
+})
+map.addLayer(dragLayer)
+
+const dragnDrop = new ol.interaction.DragAndDrop({
+  formatConstructors:[ol.format.GeoJSON],
+  source:dragSource
+})
+
+dragnDrop.on('addfeatures', function(evt){
+  console.log('added new feature')
+})
+// map.addInteraction(dragnDrop)
+
+
+// Draw Interaction
+const drawSource = new ol.source.Vector()
+
+const drawLayer = new ol.layer.Vector({
+  source: drawSource
+})
+map.addLayer(drawLayer)
+let draw = new ol.interaction.Draw({
+  source: drawSource,
+  type:"Polygon",
+  minPoints:4,
+  freehand:false
+})
+
+draw.on('drawend', function(evt){
+  drawSource.clear()
+})
+
+
+map.addInteraction(draw)
+
+// 
+// Create a draw interaction where user can select the type of 
+
+
+function getInteraction() {
+  result = document.querySelector("#interaction").value;
+  console.log(result)
+  if (result == 'poly') {
+    draw = new ol.interaction.Draw({
+      source: drawSource,
+      type:"Polygon",
+    })
+    map.addInteraction(draw)
+    
+    } else if (result == 'line') {
+        draw = new ol.interaction.Draw({
+          source: drawSource,
+          type:"LineString",
+        })
+        map.addInteraction(draw)
+
+    } else if (result == 'free') {
+      draw = new ol.interaction.Draw({
+        source: drawSource,
+        freehand:true
+      })
+      map.addInteraction(draw)
     }
-  },
-});
-
-map.addLayer(extLayer);
-
-// Create Heatmap
-// Define heatmap source
-const heatmapsource = new ol.source.Vector({
-  format: new ol.format.GeoJSON(),
-  url: "police.geojson",
-});
-
-// Define heatmap lauer
-const heatmapLayer = new ol.layer.Heatmap({
-  source: heatmapsource,
-  blur: 30,
-  gradient: ["#ff00ff", "#00f00f", "#ff0000", "#ffffff", "#000000"],
-});
-
-// add layer to map
-// map.addLayer(heatmapLayer);
-
-// Graticule
-// const graticule = new ol.Graticule({
-//   map: map,
-//   showLabels: true,
-// });
-
-/* 
-Q. Create a map having 5 lines, 3 polygons and 10 points. 
- each line should be representaion of different road. (e.g. Highway, Villege Road, Rail road,etc. )
- each polygon should be a representation of different landuse(e.g. Residential area, Garden , Lake)
- points should be a combination of Atms and bus stops and then put an image of that point as a marker
-
- */
+  }
